e0b6b301a0b2bf893310b1bbc64dd2e3641242bc
Algebar347 <kwu7@outlook.com>
2025-01-29 13:01:09 +0800

    toss coin code

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
+import random
 
-@register("helloworld", "Your Name", "一个简单的 Hello World 插件", "1.0.0")
+@register("tosscoin", "Esther", "一个简单的赛博抛硬币插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
     
-    # 注册指令的装饰器。指令名为 helloworld。注册成功后，发送 `/helloworld` 就会触发这个指令，并回复 `你好, {user_name}!`
-    @filter.command("helloworld")
-    async def helloworld(self, event: AstrMessageEvent):
+    # 注册指令的装饰器。指令名为抛硬币
+    @filter.command("抛硬币")
+    async def tosscoin(self, event: AstrMessageEvent):
         user_name = event.get_sender_name()
-        yield event.plain_result(f"Hello, {user_name}!") # 发送一条纯文本消息
+        result = random.choice(["正面", "反面"])
+        yield event.plain_result(f"{user_name}抛出一枚硬币，结果是：\n{result}\n想必您已经有了自己的答案。")
