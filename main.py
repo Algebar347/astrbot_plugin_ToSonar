7b08b1dffcd88138df6fc32a0653c197eeebc0c2
Algebar347 <kwu7@outlook.com>
2025-02-01 23:08:42 +0800

    fix function

--- main.py
+++ main.py
@@ -11,16 +11,8 @@ class MyPlugin(Star):
 
     # 注册指令的装饰器。指令名为抛硬币
     @filter.command("抛硬币")
-    async def tosscoin(self, event: AstrMessageEvent):
-        from aiocqhttp import CQHttp
-        if event.get_message_type() == MessageType.GROUP_MESSAGE:
-            group_id = event.message_obj.group_id
-            user_id = event.get_sender_id()
-            aiocqhttp_client: CQHttp = self.context.platform_manager.platform_insts[0].bot
-            assert isinstance(aiocqhttp_client, CQHttp)
-            
+    async def tosscoin(self, event: AstrMessageEvent):       
         user_name = event.get_sender_name()
-
         result = random.choice(["正面", "反面"])
         yield event.plain_result(f"{user_name}抛出一枚硬币，结果是：\n{result}\n想必您已经有了自己的答案。")
     
