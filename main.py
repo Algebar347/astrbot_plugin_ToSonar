466139ffbac40487f68c1b683ae7b15125d05024
Algebar347 <kwu7@outlook.com>
2025-02-02 01:34:55 +0800

    tosscoin update

--- main.py
+++ main.py
@@ -1,35 +1,41 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
-from astrbot.api.all import *
+from astrbot.core.platform.message_type import MessageType
 import random
 from aiocqhttp import CQHttp
 
-@register("tosscoin", "Esther", "一个简单的赛博抛硬币插件", "1.0.0")
+@register("astrbot_plugin_", "Esther", "一个简单的赛博抛硬币插件", "1.0.0")
 class MyPlugin(Star):
     def __init__(self, context: Context):
         super().__init__(context)
         self.aiocqhttp_client: CQHttp = self.context.platform_manager.platform_insts[0].bot
         assert isinstance(self.aiocqhttp_client, CQHttp)
 
-    # 注册指令的装饰器。指令名为抛硬币
-    @filter.command("抛硬币")
-    async def tosscoin(self, event: AstrMessageEvent):       
-        user_name = event.get_sender_name()
-        result = random.choice(["正面", "反面"])
-        yield event.plain_result(f"{user_name}抛出一枚硬币，结果是：{result}。")
-    
+    #获取群成员信息
+    #可用信息包括: group_id, user_id, nickname, card, sex, age, area, level, qq_level, 
+    # join_time, last_sent_time, role
     async def get_group_member_info(self, group_id: int, user_id: int):
         return await self.aiocqhttp_client.api.call_action(
             'get_group_member_info',
             group_id=group_id,
             user_id=user_id
         )
-
-    @filter.command('test')
-    async def test(self, event: AstrMessageEvent):
+    
+    # 注册指令的装饰器。指令名为抛硬币
+    @filter.command("抛硬币")
+    async def tosscoin(self, event: AstrMessageEvent):       
         if event.get_message_type() == MessageType.GROUP_MESSAGE:
             group_id = event.message_obj.group_id
             user_id = event.get_sender_id()
             member_info = await self.get_group_member_info(group_id, user_id)
-            member_info_text =str(member_info)
-            yield event.plain_result(member_info_text)
+            member_card = member_info.get('card')
+            user_name = member_card if member_card else event.get_sender_name()
+        else:
+            user_name = event.get_sender_name()
+            group_id = event.get_sender_id()
+        outcomes = ["正面", "反面", "立起来了！"]
+        weights = [0.497, 0.497, 0.006]  # 49.7% + 49.7% + 0.6% = 100%
+        result = random.choices(outcomes, weights=weights)[0]
+        if result == "立起来了！":
+            await event.plain_result(f"{user_name}抛出一枚硬币，震惊地看着它稳稳地立在了桌子上——这是什么鬼？")
+        yield event.plain_result(f"刹那间，{user_name}的硬币在第{group_id}维空间表现出——\n{result}的绝对真实！")
